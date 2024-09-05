938e5798f675b5e75ff771c7aef0a476fa603718
Seto33 <hakobjanyansedrak@gmail.com>
2024-09-05 22:04:19 +0400

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+import Fullburger from 'fullburger';
+import 'fullburger/style';
+
+new Fullburger("header");
+
+
+
+
+// slider
+function slider(elem) {
+	const slider = document.querySelector(`[data-slider="${elem}"]`);
+	const sliderList = slider.querySelector(".slider__list");
+	const sliderSlides = slider.querySelectorAll(".slider__items");
+	const sliderButtonPrev = slider.querySelector(".slider__button--prev");
+	const sliderButtonNext = slider.querySelector(".slider__button--next");
+
+	sliderSlides.forEach(slide => {
+		sliderButtonPrev.addEventListener("click", () => {
+			sliderList.scrollBy({
+				left: -slide.offsetWidth,
+				behavior: "smooth",
+			});
+		});
+
+		sliderButtonNext.addEventListener("click", () => {
+			sliderList.scrollBy({
+				left: slide.offsetWidth,
+				behavior: "smooth",
+			});
+
+			if (sliderList.scrollLeft === sliderList.scrollWidth - sliderList.offsetWidth) {
+				sliderList.scrollLeft = 0;
+			}
+		});
+	});
+
+	return slider;
+};
+
+slider("artefacts");
